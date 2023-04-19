23aa4fcd0f8e67ea7f4a47c00d9e9314d4fe1a76
wl8mes <126356991+wl8mes@users.noreply.github.com>
2023-04-19 17:12:33 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['21277235022'] //ur owner number
+global.ownername = "wl8mes" //ur owner name
+global.ytname = "YT: wl8mes" //ur yt chanel name
+global.socialm = "GitHub: wl8mes" //ur github or insta name
+global.location = "Africa, moroco, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "21277235022" //ur number
+global.premium = ['21277235022'] //ur premium number
+global.botname = '❤️wl8mes ᗷOT❤️' //ur bot name
+global.linkz = "https://youtube.com/@wl8mes" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://youtube.com/@wl8mes' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣ميامورا يروحي⍣" //ur sticker watermark packname
+global.author = "⍣0677235022⍣" //ur sticker watermark author
+global.wm = "❤️wl8mes ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
